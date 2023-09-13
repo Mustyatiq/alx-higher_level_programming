2fb7e43296a12d8600edf31a46154b6acebe3e91
Mustapha Atiku <atikumusti@gmail.com>
2023-09-14 00:15:37 +0100

    changes made

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,10 +5,10 @@ import calculator_1 as calc
 def main():
     a = 10
     b = 5
-    print(a, " + ", b, " = ", calc.add(a, b))
-    print(a, " - ", b, " = ", calc.sub(a, b))
-    print(a, " * ", b, " = ", calc.mul(a, b))
-    print(a, " / ", b, " = ", calc.div(a, b))
+    print(a, "+", b, "=", calc.add(a, b))
+    print(a, "-", b, "=", calc.sub(a, b))
+    print(a, "*", b, "=", calc.mul(a, b))
+    print(a, "/", b, "=", calc.div(a, b))
 
 
 if __name__ == "__main__":
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,13 +5,13 @@ import sys
 def main():
     if len(sys.argv) >= 2:
         if len(sys.argv) == 2:
-            print(len(sys.argv) - 1, " argument:")
+            print(len(sys.argv) - 1, "argument:")
         else:
-            print(len(sys.argv) - 1, " arguments:")
+            print(len(sys.argv) - 1, "arguments:")
         for i in range(1, len(sys.argv)):
-            print(i, ": ", sys.argv[i])
+            print(i, ": ", sys.argv[i],sep="")
     else:
-        print(len(sys.argv) - 1, " arguments.")
+        print(len(sys.argv) - 1, "arguments.")
 
 
 if __name__ == "__main__":
